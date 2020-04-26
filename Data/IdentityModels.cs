a9a46dd31777b7995fa5a84c21ddb9125df96645
emmcgill <mcgill.erik@gmail.com>
2020-04-26 19:00:57 -0400

    Updated Identity Models, Connection String, & added User Class

--- Data/IdentityModels.cs
+++ Data/IdentityModels.cs
@@ -1,10 +1,16 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using static Data.ApplicationDbContext.IdentityUserLoginConfiguration;
+using Data.Entities;
 
-namespace SMAPI.Models
+namespace Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -24,10 +30,47 @@ namespace SMAPI.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<User> AppUsers { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+
+
+        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+        {
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
+
+            public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+            {
+                public IdentityUserRoleConfiguration()
+                {
+                    HasKey(iur => iur.UserId);
+                }
+            }
+        }
+
+
     }
 }
