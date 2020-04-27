9e9926ec5f427179d2ca3f92da51e4545742cbac
Loretta Tappan <lorettatappan@gmail.com>
2020-04-27 19:44:09 -0400

    comment to break it

--- Data/IdentityModels.cs
+++ Data/IdentityModels.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Options;
 namespace Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser commenting to break 
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
