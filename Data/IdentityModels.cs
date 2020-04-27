8685e45a086cb76c25065da1ecbdbb1809a5bc52
SMackeyCoding <59894617+SMackeyCoding@users.noreply.github.com>
2020-04-27 17:21:19 -0400

    Post, Like, & IdentityModels
    Post - Added properties
    Like - "                            "
    IdentityModels - fixed using statement causing "ambiguous reference"

--- Data/IdentityModels.cs
+++ Data/IdentityModels.cs
@@ -1,14 +1,14 @@
-﻿using System.Data;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.CodeDom;
 using System.Data.Entity;
-using System.Data.Entity.Core.Common.CommandTrees;
 using System.Data.Entity.ModelConfiguration;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Net.NetworkInformation;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using static Data.ApplicationDbContext.IdentityUserLoginConfiguration;
-using Data.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Extensions.Options;
 
 namespace Data
 {
@@ -30,17 +30,18 @@ namespace Data
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-
+        
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
 
-        public DbSet<User> AppUsers { get; set; }
+        public DbSet<User> Users  { get; set; }
         public DbSet<Post> Posts { get; set; }
-        //public DbSet<Comment> Comments { get; set; }
-        //public DbSet<Reply> Replies { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies{ get; set; }
         public DbSet<Like> Likes { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -53,24 +54,27 @@ namespace Data
                 .Add(new IdentityUserLoginConfiguration())
                 .Add(new IdentityUserRoleConfiguration());
         }
-
-
-        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
-        {
-            public IdentityUserLoginConfiguration()
-            {
-                HasKey(iul => iul.UserId);
-            }
-
-            public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
-            {
-                public IdentityUserRoleConfiguration()
-                {
-                    HasKey(iur => iur.UserId);
-                }
-            }
-        }
-
-
     }
+ 
+
+
+
+
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
+    }
+
 }
